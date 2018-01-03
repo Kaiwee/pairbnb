245086bf7c107df32be6db7a84850c0f491a582c
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-03 14:13:22 +0800

    set up index and show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:email, :password, :image, :remove_image)
+		params.require(:user).permit(:email, :password)
 	end
 end
