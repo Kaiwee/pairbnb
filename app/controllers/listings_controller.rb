9e6f08f28f877df0d179064e4fd7c9b63d05ee98
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-03 15:12:13 +0800

    set up new,create and destroy actions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,4 +10,28 @@ class ListingsController < ApplicationController
 	def new
 		@listing = Listing.new
 	end
+
+	def edit
+		@listing = Listing.find(params[:id])
+	end
+
+	def create
+		@listing = current_user.listings.new(listing_params)
+		if @listing.save
+			redirect_to @listing
+		else
+			render "new"
+		end
+	end
+
+	def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listings_path
+end
+
+	private
+	def listing_params
+		params.require(:listing).permit(:title, :address, :price)
+	end
 end
