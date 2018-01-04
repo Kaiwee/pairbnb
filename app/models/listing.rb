e191dcc88bdd99dc7fae4c5b77efde14aef77266
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-04 10:38:17 +0800

    declare roles

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,6 @@ class Listing < ApplicationRecord
 
 	validates :title, :address, :price, presence: true
 	validates :price, numericality: { greater_than: 0 }
+
+	enum status: { customer: 0, moderator: 1, superadmin: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103015041) do
+ActiveRecord::Schema.define(version: 20180104020711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180103015041) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
