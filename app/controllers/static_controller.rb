485fe0825b88d46217a766f58034e5d7c95579fd
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-04 18:40:16 +0800

    add bootstrap

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,9 @@
 class StaticController < ApplicationController
+
+	def index
+		@listings = Listing.all.paginate(page: params[:page], per_page: 2).order('created_at DESC')
+	end
+
 	def about
 	end
 end
