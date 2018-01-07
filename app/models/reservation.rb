88f211fbb8a0c4fb35bd2937595b7162231e2d2d
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-07 11:55:21 +0800

    can prevent reservation date before today

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,6 +7,7 @@ class Reservation < ApplicationRecord
 
   validate :check_overlapping_dates
   validate :check_max_num_guests
+  validate :valid_date # prevent reservation date before today
 
   def check_overlapping_dates
   end
@@ -14,5 +15,13 @@ class Reservation < ApplicationRecord
   def check_max_num_guests
   	return if num_guests <= listing.max_num_guests
   	errors.add(:Maximum_number_of_guests_is_exceeded, "")
+  end
+
+  def valid_date
+  	if start_date > DateTime.now and end_date > start_date
+  		return 
+  	else
+  		errors.add(:Dates_are_invalid, "")
+  	end
   end
 end
