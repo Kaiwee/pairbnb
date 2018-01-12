6b8531bdc5755d473c0822299ea4c1c71656593f
Kaiwee <kaiweepuah0121@hotmail.com>
2018-01-12 12:53:03 +0800

    add leiga url for email

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,7 +1,7 @@
 class ReservationMailer < ApplicationMailer
 	def booking_email(reservation)
 		@reservation = Reservation.find_by(id: reservation.id)
-		@url = "http://localhost:3000/reservations/#{@reservation.id}"
+		@url = "https://leiga.herokuapp.com/reservations/#{@reservation.id}"
 
 		mail(to: @reservation.user.email, subject: 'Your reservation is booked!')
 	end
@@ -9,14 +9,14 @@ class ReservationMailer < ApplicationMailer
 	def cancellation_email(reservation_title, user, listing_id)
 		@reservation_title = reservation_title
 		@user = user
-		@url = "http://localhost:3000/listings/#{listing_id}"
+		@url = "https://leiga.herokuapp.com/listings/#{listing_id}"
 		mail(to: @user.email, subject: 'Your reservation is cancelled!')
 	end
 
 	def host_email(customer, host, listing)
 		@customer = customer
 		@host = User.find(listing.user_id)
-		@url = "http://localhost:3000/listings/#{listing.id}"
+		@url = "https://leiga.herokuapp.com/listings/#{listing.id}"
 		mail(to: @host.email, subject: 'A booking has been made!')
 	end
 
